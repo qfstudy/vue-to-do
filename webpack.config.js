d8a9e0810aa0f252d4bb1c93bb28dcf2c48e1dd7
qfstudy <qifeistudy@gmail.com>
2019-01-22 22:41:48 +0800

    配置webapck

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HTMLPlugin=require('html-webpack-plugin')
 const webpack=require('webpack')
+const ExtractPlugin=require('extract-text-webpack-plugin')
 
 const isDev = process.env.NODE_ENV==='development'
 
@@ -8,7 +9,7 @@ const config={
   target: 'web',
   entry: path.join(__dirname,'src/index.js'),
   output:{
-    filename: 'bundle.js',
+    filename: 'bundle.[hash:8].js',
     path: path.join(__dirname,'dist')
   },
   module: {
@@ -22,27 +23,27 @@ const config={
         test: /\.jsx$/,
         loader: 'babel-loader'
       },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.styl$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: true,
-            }
-          },
-          'stylus-loader'
-        ]
-      },
+      // {
+      //   test: /\.css$/,
+      //   use: [
+      //     'style-loader',
+      //     'css-loader'
+      //   ]
+      // },
+      // {
+      //   test: /\.styl$/,
+      //   use: [
+      //     'style-loader',
+      //     'css-loader',
+      //     {
+      //       loader: 'postcss-loader',
+      //       options: {
+      //         sourceMap: true,
+      //       }
+      //     },
+      //     'stylus-loader'
+      //   ]
+      // },
       {
         test: /\.(gif|jpg|jpeg|png|svg)$/,
         use: [
@@ -68,6 +69,20 @@ const config={
 }
 
 if(isDev){
+  config.module.rules.push({
+    test: /\.styl$/,
+    use: [
+      'style-loader',
+      'css-loader',
+      {
+        loader: 'postcss-loader',
+        options: {
+          sourceMap: true,
+        }
+      },
+      'stylus-loader'
+    ]
+  },)
   config.devtool = '#cheap-module-eval-source-map'
   config.devServer={
     port: 8000,
@@ -82,6 +97,29 @@ if(isDev){
   config.plugins.push(
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin()
+  )
+}else{
+  config.output.filename = '[name].[chunkhash:8].js'
+  config.module.rules.push(
+    {
+      test: /\.styl/,
+      use: ExtractPlugin.extract({
+        fallback: 'style-loader',
+        use:[
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+            }
+          },
+          'stylus-loader'
+        ]
+      })
+    },
+  )
+  config.plugins.push(
+    new ExtractPlugin('styles.[contentHash:8].css')
   )
 }
 
